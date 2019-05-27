452af5055a64f056eb3354f840e66d9e9b0fc9ab
jerry <610819267@qq.com>
2019-05-27 17:33:04 +0800

    update

--- 01-hello-webpack/webpack.config.js
+++ 01-hello-webpack/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'development',
     entry: './src/hello.js',
     output: {
-        filename: 'hello.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 }
